cf5ebc10bc9c355e84abc5e131fe1cf7aa54f09d
BrasMonkey <112280149+BrasMonkey@users.noreply.github.com>
2022-11-11 22:26:56 +0100

    get search criteria

--- VKinder.py
+++ VKinder.py
@@ -1,6 +1,7 @@
 from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardButton, VkKeyboardColor
 import requests
 from pprint import pprint
 
@@ -46,39 +47,56 @@ class User:
             'age_from': get_age(self.id, "Укажи минимальный возраст твоей идеальной половинки"),      # через общение с ботом просим ввести желаемый возраст
             'age_to': get_age(self.id, "И каков твой максимальный порог? :)"),
             'sex': get_sex(self.id, "Кто тебя интересует: мужчины или женщины?"),
+            #'sex': button("Кто тебя интересует: мужчины или женщины?", 'мужчины'),
+            'has_photo': '1',
+            'can_access_closed': False,
             'v': '5.131'
         }
-        pprint(requests.get(URL, params=params).json())
-
-
+        print("getuserlist done")
+        return requests.get(URL, params=params).json()
+class Bot:
+    pass
 # Шаблон для отправки сообщений
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+def write_msg(user_id, message, keyboard=None):
+    params ={
+        'user_id': user_id,
+        'message': message,
+        'random_id': randrange(10 ** 7)
+    }
+    if keyboard != None:
+        params['keyboard']: keyboard.get_keyboard()
+    else:
+        params=params
+    vk.method('messages.send', params)
 
-def send_profile(user_id):
-    profile = 0
-    vk.method('messages.send', {'user_id': user_id, 'message': profile,  'random_id': randrange(10 ** 7),})
+def send_profile(prof):
+    profile = (f"Имя: {prof['name']}\nФамилия: {prof['surname']}")
+    vk.method('messages.send', {'user_id': main_user.id, 'message': profile,  'random_id': randrange(10 ** 7),})
+    photo_list = prof["photos"]
+    print(photo_list)
+    for pic in photo_list:
+        vk.method('messages.send', {'user_id': main_user.id, 'attachment': f"photo{prof['user']}_{pic}", 'random_id': randrange(10 ** 7), })
+
 
 # шаблон для приветствия/прощания
 def send_basic_msg(user):
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
-                request = event.text
+                request = event.text.lower()
 
                 if request == "привет":
                     write_msg(event.user_id, f"Хай, {main_user.name}. Я - бот, который поможет подобрать для тебя вторую половинку! "
                                              f"\nДля начала я задам тебе пару вопросов :)")
-                    user.get_userlist()
+
                 elif request == "пока":
                     write_msg(event.user_id, "Пока((")
                 else:
                     write_msg(event.user_id, "Не поняла вашего ответа...")
+            break
 
-
-def get_age(id, message): #как зациклить функцию
+def get_age(id, message):
     write_msg(id, message)
-
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             request = event.text
@@ -88,8 +106,6 @@ def get_age(id, message): #как зациклить функцию
                 write_msg(id, "Введи возраст цифрами")
             else:
                 return request
-
-
 
 def get_sex(id, message):
     write_msg(id, message)
@@ -106,7 +122,66 @@ def get_sex(id, message):
                     get_sex(id, "Так все же парни или девушки?")
                     continue
 
+def button(message, lable):
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            text = event.text
+
+            if text == message:
+                keyboard = VkKeyboard(one_time=True)
+                keyboard.add_button(label=lable, color=VkKeyboardColor.POSITIVE)
+                write_msg(main_user.id, "", keyboard)
+
+
+
+def get_photos(user_id):
+    URL = "https://api.vk.com/method/photos.getAll"
+    params = {
+        'access_token': main_user.token,
+        'owner_id': user_id,
+        'extended': "1",
+        'v': "5.131"
+        }
+    result = requests.get(URL, params).json()
+    result = result['response']['items']
+    ph_list = sorted(result, key=lambda row: row['likes']['count'])
+    bestphotos = ph_list[-1:-4:-1]
+    bestphotos = sorted(bestphotos, key=lambda row:row['sizes'][0]['height'])
+    # urls = [url['sizes'][-1]['url'] for url in bestphotos]
+    urls = [url['id'] for url in bestphotos]
+
+    return urls
+
+def get_profile(profiles):
+
+    for element in profiles['response']['items']:
+        try:
+            profile = {
+                "name": element['first_name'],
+                'surname': element['last_name'],
+                'photos': get_photos(element['id']),
+                'user': element['id']
+            }
+            print(profile)
+            send_profile(profile)
+            vk.method('messages.send', {'user_id': main_user.id, 'message': "да/нет?",
+                                        'random_id': randrange(10 ** 7), })
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.to_me:
+                        request = event.text.lower()
+                        if request == "нет":
+                            break
+                        else:
+                            break
+
+        except:
+            continue
+        print("----------")
+
+
 
 main_user = User()
-pprint(main_user.get_info)
+#pprint(main_user.get_info)
 send_basic_msg(main_user)
+pprint(get_profile(main_user.get_userlist()))
